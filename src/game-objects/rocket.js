77edd445b2ece45281fcae99e60c75c358aec6f7
Harsh <verma_harsh@outlook.com>
2020-02-23 18:57:59 +0530

    Source formatting

--- src/game-objects/rocket.js
+++ src/game-objects/rocket.js
@@ -1,29 +1,33 @@
-import React from 'react';
+import React from "react";
 export class Rocket {
-    x;
-    y;
-    speed; //TODO: will introduce spped later
+  x;
+  y;
+  speed; //TODO: will introduce spped later
 
+  constructor() {
+    this.x = window.innerWidth / 2;
+    this.y = window.innerHeight / 2;
+    this.captureKeyPress();
+  }
 
-    constructor(){
-        this.x = window.innerWidth/2;
-        this.y = window.innerHeight/2;
-        this.captureKeyPress();
-    }
+  captureKeyPress() {
+    document.addEventListener("keypress", () => {
+      this.x = 2;
+      this.y = 2;
+    });
+  }
 
-    captureKeyPress(){
-        
-    }
-
-    getMyStructure(){
-        return (
-            <div style={{
-                position:'absolute',
-                top: this.y,
-                left: this.x
-            }}>
-                Rocket
-            </div>
-        )
-    }
+  getMyStructure() {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: this.y,
+          left: this.x
+        }}
+      >
+        Rocket
+      </div>
+    );
+  }
 }
