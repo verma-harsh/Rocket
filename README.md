4bed1bccbfb308a5a0491f21e11f63c12547314c
Harsh <verma_harsh@outlook.com>
2021-02-19 00:07:37 +0530

    can now pause and play

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 A small game based on javascript with react.
 
 Currently I am in middle of developing it. Soon I will be making a github page for it
+
 # TODO
+
 - [x] ~~Make the rocket move~~
 - [x] ~~Make some awesome background~~
 - [x] ~~Add a score counter~~
 - [x] ~~Add rotation while changing directions~~
+- [x] ~~Add Pause Play button~~
+- [ ] Make pause play stylish
 - [ ] Make a start menu
 - [ ] A settings option would seems nice
-- [ ] Add physics 
+- [ ] Add physics
 - [ ] Add enemy ships/rocks/"something enemyish"
 - [ ] Move this project to github pages/heroku
+
 # Build Project
-* npm run build
+
+- npm run build
 
 # Start Application
-* npm run start
-    
+
+- npm run start
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,43 +1,58 @@
-import React from 'react';
+import React from "react";
 import { Rocket } from "../../game-objects/rocket/rocket";
 import { Star } from "../../game-objects/star/Star";
 import { Score } from "../../game-objects/score/score";
 import "../app/app.css";
 
 class RocketApp extends React.Component {
-    rocket = new Rocket();
-    state = {
-        framePos : 0,
-        score: 0
-    }
+  rocket = new Rocket();
+  state = {
+    framePos: 0,
+    score: 0,
+    pausePlayText: "Pause",
+  };
+  isPaused = false;
 
-    componentDidMount(){
+  componentDidMount() {
+    this.gameLoop();
+  }
+
+  gameLoop() {
+    this.setState({
+      framePos: this.state.framePos > 100 ? 0 : this.state.framePos + 0.1,
+      score: this.state.score + 0.01,
+    });
+    window.requestAnimationFrame(() => {
+      if (!this.isPaused) {
         this.gameLoop();
-    }
+      }
+    });
+  }
 
-    gameLoop(){
-        this.setState({
-            framePos : this.state.framePos > 100 ? 0 : (this.state.framePos + 0.1),
-            score : (this.state.score + 0.01)
-        })
-        window.requestAnimationFrame(()=>{
-            this.gameLoop();
-        })  
-    }   
-    
-    render(){
-        return (
-            <div className="full-height">
-                <Score score={parseInt(this.state.score)}/>
-                <div className="full-height space-background">
-                    <Star framePos={this.state.framePos} />
-                </div>
-               <div>
-                <Rocket framePos={this.state.framePos}/>
-               </div>
-             </div>
-        )
-    }
+  _pausePlay() {
+    this.isPaused = !this.isPaused;
+    const pausePlayText = this.isPaused ? "Play" : "Pause";
+    this.setState({
+      ...this.state,
+      pausePlayText,
+    });
+    !this.isPaused && this.gameLoop();
+  }
+
+  render() {
+    return (
+      <div className="full-height">
+        <div onClick={() => this._pausePlay()}>{this.state.pausePlayText}</div>
+        <Score score={parseInt(this.state.score)} />
+        <div className="full-height space-background">
+          <Star framePos={this.state.framePos} />
+        </div>
+        <div>
+          <Rocket framePos={this.state.framePos} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RocketApp;
